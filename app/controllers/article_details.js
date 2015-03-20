f5ddc832fa27cb8f983102fa46d277cffb65cf5b
Vagner Guedes <rj.vag.ner@gmail.com>
2015-03-20 02:10:11 -0300

    Tela de detalhes dividida.

--- app/controllers/article_details.js
+++ app/controllers/article_details.js
@@ -8,46 +8,35 @@ var data_css =  "<head><style>" +
 
 $.article.text = article;
 
-var db = Ti.Database.open('_alloy_');
 
-var article_what_is_RS = db.execute('SELECT body FROM article_what_is WHERE article_id = ' + articleId);
-var article_what_is_body = article_what_is_RS.fieldByName('body');
-$.whatis.html = data_css + article_what_is_body;
+var openDetail = function(e) {
+	var db = Ti.Database.open('_alloy_');
+	
+	var detail_name = e['source']['id'];
+	var detail_description = e['source']['title'];
+	var rs = db.execute("SELECT body FROM article_" + detail_name + " WHERE article_id = " + articleId);
+	var body = rs.isValidRow() ? rs.fieldByName('body') : "Vazio";
+	
+	rs.close();
+	db.close();
+	
+	var detail_args = {
+		article_id: articleId,
+		article_name: article,
+		detail_name: detail_name,
+		detail_description: detail_description,
+		detail_HTML: body
+	};
 
-var article_symptoms_RS = db.execute('SELECT body FROM article_symptoms WHERE article_id = ' + articleId);
-var article_symptoms_body = article_symptoms_RS.fieldByName('body');
-$.symptoms.html = article_symptoms_body;
+	var detail_view = Alloy.createController("detail", detail_args).getView();
+    // if (OS_IOS) {
+        // $.navGroupWin.openWindow(saudeaz_view);
+    // }
+ 	// if (OS_ANDROID) {
+ 		detail_view.addEventListener('open', function(e) {
+   			detail_view.activity.actionBar.hide();
+		});
+        detail_view.open();
+	// }	
 
-var article_diagnosis_RS = db.execute('SELECT body FROM article_diagnosis WHERE article_id = ' + articleId);
-var article_diagnosis_body = article_diagnosis_RS.fieldByName('body');
-$.diagnosis.html = article_diagnosis_body;
-
-var article_duration_RS = db.execute('SELECT body FROM article_duration WHERE article_id = ' + articleId);
-var article_duration_body = article_duration_RS.fieldByName('body');
-$.duration.html = article_duration_body;
-
-var article_prevention_RS = db.execute('SELECT body FROM article_prevention WHERE article_id = ' + articleId);
-var article_prevention_body = article_prevention_RS.fieldByName('body');
-$.prevention.html = article_prevention_body;
-
-var article_treatment_RS = db.execute('SELECT body FROM article_treatment WHERE article_id = ' + articleId);
-var article_treatment_body = article_treatment_RS.fieldByName('body');
-$.treatment.html = article_treatment_body;
-
-var article_when_contact_professional_RS = db.execute('SELECT body FROM article_when_contact_professional WHERE article_id = ' + articleId);
-var article_when_contact_professional_body = article_when_contact_professional_RS.fieldByName('body');
-$.when_contact_professional.html = article_when_contact_professional_body;
-
-var article_prognosis_RS = db.execute('SELECT body FROM article_prognosis WHERE article_id = ' + articleId);
-var article_prognosis_body = article_prognosis_RS.fieldByName('body');
-$.prognosis.html = article_prognosis_body;
-
-var article_aditional_info_RS = db.execute('SELECT body FROM article_aditional_info WHERE article_id = ' + articleId);
-var article_aditional_info_body = article_aditional_info_RS.fieldByName('body');
-$.aditional_info.html = article_aditional_info_body;
-
-article_what_is_RS.close();
-
-// var article_!_RS = db.execute('SELECT body FROM article_! WHERE article_id = ' + articleId);
-// var article_!_body = article_!_RS.fieldByName('body');
-// $.!.html = article_!_body;
+};
