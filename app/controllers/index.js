9b7f7e3bb14cb5510c540415b5cc128db372cda7
Vagner Guedes <rj.vag.ner@gmail.com>
2015-04-15 12:19:28 -0300

    Teste com dados de Saúde da Criança

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,7 +16,17 @@ function load_exames() {
 	alert("exames");
 }
 function load_saude_crianca() {
-	alert("saude crianca");
+	var articlesView = Alloy.createController("articles", {'articlesSession': 'crianca'}).getView();
+    // if (OS_IOS) {
+        // $.navGroupWin.openWindow(saudeaz_view);
+    // }
+ 	// if (OS_ANDROID) {
+ 		articlesView.addEventListener('open', function(e) {
+   			articlesView.activity.actionBar.hide();
+		});
+        articlesView.open();
+    // }    
+
 }
 function load_saude_az() {
 	var saudeaz_view = Alloy.createController("articles", {}).getView();
