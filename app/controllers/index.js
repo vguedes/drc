f3239e01056599054d29d93dbbadb7f16171a066
Vagner Guedes <rj.vag.ner@gmail.com>
2015-04-26 20:03:54 -0300

    Tela inicial Barra superior, Botão marcação de consulta Rows para botões de conteúdo

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,55 +1,35 @@
 var db = Ti.Database.install('/dr.db', '_alloy_');
 db.close();
 
-// if (OS_IOS) {
-	// $.navGroupWin.open();
-// }
-// if (OS_ANDROID) {
 $.index.addEventListener('open', function(e) {
     $.index.activity.actionBar.hide();
 });
-	$.index.open();
+$.index.open();
+
+
+// function load_exames() {
+	// alert("exames");
 // }
-
-
-function load_exames() {
-	alert("exames");
-}
-function load_saude_crianca() {
-	var articlesView = Alloy.createController("articles", {'articlesSession': 'crianca'}).getView();
-    // if (OS_IOS) {
-        // $.navGroupWin.openWindow(saudeaz_view);
-    // }
- 	// if (OS_ANDROID) {
- 		articlesView.addEventListener('open', function(e) {
-   			articlesView.activity.actionBar.hide();
-		});
-        articlesView.open();
-    // }    
-
-}
-function load_saude_az() {
-	var saudeaz_view = Alloy.createController("articles", {}).getView();
-    // if (OS_IOS) {
-        // $.navGroupWin.openWindow(saudeaz_view);
-    // }
- 	// if (OS_ANDROID) {
- 		saudeaz_view.addEventListener('open', function(e) {
-   			saudeaz_view.activity.actionBar.hide();
-		});
-        saudeaz_view.open();
-    // }    
-} 
-
-function load_gravidez() {
-	var saudeaz_view = Alloy.createController("onboarding", {}).getView();
-    // if (OS_IOS) {
-        // $.navGroupWin.openWindow(saudeaz_view);
-    // }
- 	// if (OS_ANDROID) {
- 		saudeaz_view.addEventListener('open', function(e) {
-   			saudeaz_view.activity.actionBar.hide();
-		});
-        saudeaz_view.open();
-    // } 
-}
+// function load_saude_crianca() {
+	// var articlesView = Alloy.createController("articles", {'articlesSession': 'crianca'}).getView();
+ 		// articlesView.addEventListener('open', function(e) {
+   			// articlesView.activity.actionBar.hide();
+		// });
+        // articlesView.open();
+// 
+// }
+// function load_saude_az() {
+	// var saudeaz_view = Alloy.createController("articles", {}).getView();
+ 		// saudeaz_view.addEventListener('open', function(e) {
+   			// saudeaz_view.activity.actionBar.hide();
+		// });
+        // saudeaz_view.open();
+// } 
+// 
+// function load_gravidez() {
+	// var saudeaz_view = Alloy.createController("onboarding", {}).getView();
+ 		// saudeaz_view.addEventListener('open', function(e) {
+   			// saudeaz_view.activity.actionBar.hide();
+		// });
+        // saudeaz_view.open();
+// }
