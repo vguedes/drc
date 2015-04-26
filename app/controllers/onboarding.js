0909ea7043565af117fdfdede7954f052de139d5
Vagner Guedes <rj.vag.ner@gmail.com>
2015-04-26 17:23:24 -0300

    Onboardnig digitar confirmação e tela de aguardo.

--- app/controllers/onboarding.js
+++ app/controllers/onboarding.js
@@ -1,1 +1,16 @@
 var args = arguments[0] || {};
+
+function sendVerificationCode() {
+	$.sbt1.text = "";
+	$.sbt2.text = "Validando. Por favor aguarde...";
+	$.input_phone_number.hide();
+	$.round_btn.height = 0;
+	$.wait.show();
+}
+
+function sendPhoneNumber() {
+	$.sbt1.text = "Para completar a validação,";
+	$.sbt2.text = "digite abaixo o código enviado por SMS.";
+	$.input_phone_number.value = "";
+	$.next_btn.addEventListener('click', sendVerificationCode);
+}
