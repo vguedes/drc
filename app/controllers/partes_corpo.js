4902c3e12c096a4ad90b89bd16c9576e4e5b31ef
Vagner Guedes <rj.vag.ner@gmail.com>
2015-04-29 05:11:51 -0300

    Integração da tela Partes do corpo com a tela de listagem de artigos.

--- app/controllers/partes_corpo.js
+++ app/controllers/partes_corpo.js
@@ -1,1 +1,6 @@
 var args = arguments[0] || {};
+
+function open_articles(e) {
+	var window_title = e.source.children[1].text;
+	var articleView = Alloy.createController("artigos", {'articlesSession': window_title, 'window_title': window_title}).getView();
+}
