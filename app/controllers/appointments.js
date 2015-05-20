ecc892acb58fdd96d22db49ea3d3c33c1788b067
Vagner Guedes <rj.vag.ner@gmail.com>
2015-05-20 19:54:36 -0300

    Trazendo os appointments do banco.

--- app/controllers/appointments.js
+++ app/controllers/appointments.js
@@ -42,7 +42,6 @@ clickable_area.addEventListener('click', function(e){
 	win.close();
 });
 
-
 var labelTitleText = Ti.UI.createLabel({
 	height: Titanium.UI.FILL,
  	color: "white",
@@ -50,10 +49,48 @@ var labelTitleText = Ti.UI.createLabel({
 	left: "72dp",zIndex:999, text:"Consultas e exames marcados"
 });
 
-viewTitle.add(labelTitleText);
 viewTitle.add(clickable_area);
-
-
+viewTitle.add(labelTitleText);
 win.add(viewTitle);
+
+
+var tableData = [];
+var db = Ti.Database.open('_alloy_');
+var query = " SELECT" +
+			"	appointments.id as appt_id," +
+			" 	appointments.date_time as appt_date_time," +
+			" 	clinic.name as clinic_name," +
+			" 	speciality.name as speciality_name" +
+			" FROM" +
+			" 	appointments" +
+			" INNER JOIN" +
+			" 	clinic" +
+			" ON" +
+			" 	appointments.clinic_id = clinic.id" +
+			" INNER JOIN" +
+			" 	speciality" +
+			" ON" +
+			" 	appointments.speciality_id = speciality.id;";
+var articlesRS = db.execute(query);
+while (articlesRS.isValidRow()) {
+	tableData.push({
+		'appt_id': articlesRS.fieldByName('appt_id'),
+		'appt_date_time': new Date(articlesRS.fieldByName('appt_date_time')),
+		'clinic_name': articlesRS.fieldByName('clinic_name'),
+		'speciality_name': articlesRS.fieldByName('speciality_name')
+	});
+  articlesRS.next();
+}
+articlesRS.close();
+db.close();
+
+console.log(tableData);
+
+
+
+
+
+
+
 
 win.open();
